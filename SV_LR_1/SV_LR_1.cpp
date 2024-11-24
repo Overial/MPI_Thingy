d591defb0e747069aed0fbe55cbb3275a05a6b15
Overial <dfjx2201@yandex.ru>
2024-11-24 21:03:15 +0300

    added matrix initialization

--- SV_LR_1/SV_LR_1.cpp
+++ SV_LR_1/SV_LR_1.cpp
@@ -3,9 +3,74 @@
 
 using namespace std;
 
+int**& InitMatrix(int InRowCount, int InColCount)
+{
+	int** Matrix = new int* [InRowCount];
+	for (int i = 0; i < InRowCount; ++i)
+	{
+		Matrix[i] = new int[InColCount];
+	}
+
+	return Matrix;
+}
+
+void FillMatrix(int**& InMatrix, int InRowCount, int InColCount)
+{
+	for (int i = 0; i < InRowCount; ++i)
+	{
+		for (int j = 0; j < InColCount; ++j)
+		{
+			InMatrix[i][j] = rand() % 10;
+		}
+	}
+}
+
+void PrintMatrix(int** InMatrix, int InRowCount, int InColCount)
+{
+	for (int i = 0; i < InRowCount; ++i)
+	{
+		for (int j = 0; j < InColCount; ++j)
+		{
+			printf("Elem[%d][%d] = %d\n", i, j, InMatrix[i][j]);
+		}
+	}
+}
+
+void DeleteMatrix(int**& InMatrix, int InRowCount)
+{
+	for (int i = 0; i < InRowCount; ++i)
+	{
+		delete[] InMatrix[i];
+	}
+
+	delete[] InMatrix;
+}
+
 int main()
 {
-	cout << "Hello, MPI!";
+	{
+		MPI_Init(NULL, NULL);
+
+		int Rank = 0;
+		int Size = 0;
+
+		MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
+		MPI_Comm_size(MPI_COMM_WORLD, &Size);
+
+		printf("Rank: %d Size: %d\n", Rank, Size);
+
+		int RowCount = 3;
+		int ColCount = 4;
+
+		int** Matrix = InitMatrix(RowCount, ColCount);
+		FillMatrix(Matrix, RowCount, ColCount);
+		PrintMatrix(Matrix, RowCount, ColCount);
+		DeleteMatrix(Matrix, RowCount);
+
+		MPI_Finalize();
+	}
+
+	_CrtDumpMemoryLeaks();
 
 	return 0;
 }
