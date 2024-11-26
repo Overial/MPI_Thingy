13fba6729c68650b9e3a1618d37860dd1d9d1398
Overial <dfjx2201@yandex.ru>
2024-11-27 02:45:42 +0300

    implemented one process matrix mult

--- SV_LR_1/SV_LR_1.cpp
+++ SV_LR_1/SV_LR_1.cpp
@@ -33,7 +33,7 @@ void PrintMatrix(int** InMatrix, int InRowCount, int InColCount)
 		for (int j = 0; j < InColCount; ++j)
 		{
 			// printf("Elem[%d][%d] = %d", i, j, InMatrix[i][j]);
-			cout << "|" << InMatrix[i][j];
+			cout << "| " << InMatrix[i][j] << " ";
 		}
 		cout << "|" << endl;
 	}
@@ -50,9 +50,70 @@ void DeleteMatrix(int**& InMatrix, int InRowCount)
 	delete[] InMatrix;
 }
 
-void Solve(int**& InMatrix1, int**& InMatrix2, int InRowCount, int ColCount, int InRank)
+int** Solve(int**& InMatrix1, int**& InMatrix2, int InRowCount1, int InColCount1, int InRowCount2, int InColCount2)
 {
-	
+	int** Result = new int* [InRowCount1];
+	for (int i = 0; i < InRowCount1; ++i)
+	{
+		Result[i] = new int[InColCount2];
+	}
+
+	Result[0][0] = 1;
+	Result[0][1] = 2;
+	Result[1][0] = 3;
+	Result[1][1] = 4;
+
+	cout << "Processing:" << endl << endl;
+	for (int i = 0; i < InRowCount1; ++i)
+	{
+		for (int j = 0; j < InColCount2; ++j)
+		{
+			cout << "| ";
+			int CurrentCol2 = 0;
+			for (int j1 = 0; j1 < InColCount1; ++j1)
+			{
+				cout << InMatrix1[i][j1] << "*" << InMatrix2[CurrentCol2][j];
+				if (j1 != InColCount1 - 1)
+				{
+					cout << " + ";
+				}
+				else
+				{
+					cout << " ";
+				}
+
+				++CurrentCol2;
+			}
+		}
+		cout << "|" << endl;
+	}
+	cout << endl;
+
+	cout << "Calculating cells:" << endl << endl;
+	for (int i = 0; i < InRowCount1; ++i)
+	{
+		for (int j = 0; j < InColCount2; ++j)
+		{
+			cout << "| ";
+
+			int Buffer = 0;
+			int CurrentCol2 = 0;
+			
+			for (int j1 = 0; j1 < InColCount1; ++j1)
+			{
+				Buffer += InMatrix1[i][j1] * InMatrix2[CurrentCol2][j];
+				++CurrentCol2;
+			}
+			
+			Result[i][j] = Buffer;
+			
+			cout << Result[i][j] << " ";
+		}
+		cout << "|" << endl;
+	}
+	cout << endl;
+
+	return Result;
 }
 
 int main()
@@ -66,23 +127,51 @@ int main()
 		MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
 		MPI_Comm_size(MPI_COMM_WORLD, &Size);
 
-		printf("Rank: %d Size: %d\n", Rank, Size);
+		printf("Rank: %d Size: %d\n\n", Rank, Size);
 
-		int RowCount = 2;
-		int ColCount = 2;
+		int RowCount1 = 2;
+		int ColCount1 = 3;
 
-		int** Matrix1 = InitMatrix(RowCount, ColCount);
-		FillMatrix(Matrix1, RowCount, ColCount);
-		PrintMatrix(Matrix1, RowCount, ColCount);
+		int** Matrix1 = InitMatrix(RowCount1, ColCount1);
+		// FillMatrix(Matrix1, RowCount, ColCount);
 
-		int** Matrix2 = InitMatrix(RowCount, ColCount);
-		FillMatrix(Matrix2, RowCount, ColCount);
-		PrintMatrix(Matrix2, RowCount, ColCount);
+		Matrix1[0][0] = 1;
+		Matrix1[0][1] = 2;
+		Matrix1[0][2] = 2;
+		Matrix1[1][0] = 3;
+		Matrix1[1][1] = 1;
+		Matrix1[1][2] = 1;
 
-		Solve(Matrix1, Matrix2, RowCount, ColCount, Rank);
+		cout << "Matrix 1:" << endl;
+		PrintMatrix(Matrix1, RowCount1, ColCount1);
 
-		DeleteMatrix(Matrix1, RowCount);
-		DeleteMatrix(Matrix2, RowCount);
+		int RowCount2 = 3;
+		int ColCount2 = 2;
+
+		int** Matrix2 = InitMatrix(RowCount2, ColCount2);
+		// FillMatrix(Matrix2, RowCount, ColCount);
+		
+		Matrix2[0][0] = 4;
+		Matrix2[0][1] = 2;
+		Matrix2[1][0] = 3;
+		Matrix2[1][1] = 1;
+		Matrix2[2][0] = 1;
+		Matrix2[2][1] = 5;
+		
+		cout << "Matrix2:" << endl;
+		PrintMatrix(Matrix2, RowCount2, ColCount2);
+
+		// Expected result:
+		// | 12 | 14 |
+		// | 16 | 12 |
+
+		int** Result = Solve(Matrix1, Matrix2, RowCount1, ColCount1, RowCount2, ColCount2);
+
+		cout << "Result:" << endl;
+		PrintMatrix(Result, RowCount1, ColCount2);
+
+		DeleteMatrix(Matrix1, RowCount1);
+		DeleteMatrix(Matrix2, RowCount2);
 
 		MPI_Finalize();
 	}
